a333b0e3091788d1aa564bdf30864d021ab34069
Benjamin Dutta <fijii360@gmail.com>
2023-06-28 17:25:24 +0530

    counter removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
